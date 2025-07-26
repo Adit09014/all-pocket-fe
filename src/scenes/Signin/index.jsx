c67f264fab4d92ee9ab373270cee35ac1c5223fe
Adit09014 <dpsvn.adit09014@gmail.com>
2025-07-26 22:12:50 +0530

    sign in url change

--- src/scenes/Signin/index.jsx
+++ src/scenes/Signin/index.jsx
@@ -71,7 +71,7 @@ export const Signin = () => {
     if (!isValid) return;
 
     try {
-      const response = await fetch('http://localhost:3000/api/auth/login', {
+      const response = await fetch('https://all-pocket-be.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',

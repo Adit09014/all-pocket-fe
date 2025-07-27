4e39404a21fa8c9bdb544b6e34486c0fea36a59a
Adit09014 <dpsvn.adit09014@gmail.com>
2025-07-27 11:20:10 +0530

    Protection Fix

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "./CheckAuth";
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated } = useContext(AuthContext);
